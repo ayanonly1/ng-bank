80fe35886524c21b27d85fa98664e44947b7461d
Ayan Pal <ayanonly1@gmail.com>
2019-10-14 02:12:42 +0530

    Fixed express issues

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express';
+const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(precess.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res){
   res.sendfile(path.join(__dirname + '/dist/index.html'));
 });
